1a42b3ac1bb63aee001f1307a2e98a69e4f6383d
Shayne <khaldi.alternance@gmail.com>
2020-03-26 10:18:21 +0100

    .travis.yml 5

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -1,5 +1,63 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.ipiecoles.java.java350.model;
 
-class EmployeTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EmployeTest extends Assertions {
+
+    @Test
+    public void testAnneeAncinneteNow(){
+        //Given Envoie de la class Employe
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now());
+
+        //When  recupéreration du nombre d'année d'ancienneté
+        Integer anneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then : test du nombre d'année d'ancienneté
+        assertThat(anneeAnciennete).isEqualTo(0);
+    }
+
+    @Test
+    public void should_retourner_annee_anciennete_employe_moins_2(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(2L));
+
+        //When
+        Integer anneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then
+        assertEquals(2, anneeAnciennete.intValue());
+    }
+
+    @Test
+    public void should_retourner_annee_anciennete_employe_null(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(null);
+
+        //When
+        Integer anneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then
+        assertEquals(0, anneeAnciennete.intValue());
+    }
+
+    @Test
+    public void should_retourner_annee_anciennete_employe_plus_3(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().plusYears(3L));
+
+        //When
+        Integer anneeAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then
+        assertEquals(0, anneeAnciennete.intValue());
+    }
 
 }
--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryTest.java
@@ -1,5 +1,22 @@
+package com.ipiecoles.java.java350.repository;
+
+import com.ipiecoles.java.java350.model.Employe;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeRepositoryTest {
+
+    @Test
+    void should_retourner_derniere_matricule_employe() {
+        //Given
+        Employe employer1 = new Employe("Khaldi", "Shaïnee", "Z1997", LocalDate.now(), 2000d, 1, 1.0);
+        //When
+        string lastMatricule = employeRepositoy.findLastMatricule();
+        //Then
+        assertThat();
+    }
 
 }
